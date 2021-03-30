025946e0b1c0121643b64d69b9b1aa44ee885d7f
Alexandru Badiu <andu@ctrlz.ro>
2021-03-30 17:17:08 +0100

    fix: added better interpolation

--- process.py
+++ process.py
@@ -31,11 +31,33 @@ device_name = header_parts[4]
 base_name = Path(input_file).stem
 
 # Read the data into a data frame
-df = pandas.read_csv(input_file, skiprows=[0], delimiter='\t')
+df = pandas.read_csv(input_file, skiprows=[0], delimiter='\t', dtype={
+  'address': str,
+  'Time(s)': str,
+  'ax(g)': float,
+  'ay(g)': float,
+  'az(g)': float,
+  'wx(deg/s)': float,
+  'wy(deg/s)': float,
+  'wz(deg/s)': float,
+  'AngleX(deg)': float,
+  'AngleY(deg)': float,
+  'AngleZ(deg)': float,
+  'T(°)': float,
+  'hx': float,
+  'hy': float,
+  'hz ': float
+})
 
 # We need to add the date to the time column so that we can parse it
 for idx,row in df.iterrows():
   df.at[idx, 'Time(s)'] = f"{start_time}{df.at[idx, 'Time(s)']}"
+  df.at[idx, 'ax(g)'] = df.at[idx, 'ax(g)'] * 9.81
+  df.at[idx, 'ay(g)'] = df.at[idx, 'ay(g)'] * 9.81
+  df.at[idx, 'az(g)'] = df.at[idx, 'az(g)'] * 9.81
+  df.at[idx, 'hx'] = df.at[idx, 'hx'] / 1000000.0
+  df.at[idx, 'hy'] = df.at[idx, 'hy'] / 1000000.0
+  df.at[idx, 'hz '] = df.at[idx, 'hz '] / 1000000.0
 
 # Parse the time into two new fields, with different granularity
 # timestamp - used for grouping, granularity in seconds
@@ -44,11 +66,8 @@ df['timestamp_e'] = pandas.to_datetime(df['Time(s)'], errors='coerce', format='%
 df['timestamp'] = pandas.to_datetime(df['Time(s)'], errors='coerce', format='%Y-%m-%d %H:%M:%S')
 df.index = df['timestamp_e']
 
-# Resample to 1ms and interpolate
-dfr = df.resample('1L').pad()
-
 # Convert each group into the EdgeImpulse format and write it to a file
-for n, g in dfr.groupby(pandas.Grouper(key='timestamp',freq=f"{seconds}S")):
+for n, g in df.groupby(pandas.Grouper(key='timestamp',freq=f"{seconds}S")):
   fname = f"{base_name}_{n.strftime('%Y_%m_%d_%H_%M_%S')}.json"
 
   data = {
@@ -80,11 +99,29 @@ for n, g in dfr.groupby(pandas.Grouper(key='timestamp',freq=f"{seconds}S")):
       "values": [],
     },
   }
-  for idx,row in g.iterrows():
+
+  # Resample to 1ms and interpolate
+  gr = g.resample('1L').mean()
+
+  gr["ax(g)"] = gr["ax(g)"].interpolate()
+  gr["ay(g)"] = gr["ay(g)"].interpolate()
+  gr["az(g)"] = gr["az(g)"].interpolate()
+  gr["wx(deg/s)"] = gr["wx(deg/s)"].interpolate()
+  gr["wy(deg/s)"] = gr["wy(deg/s)"].interpolate()
+  gr["wz(deg/s)"] = gr["wz(deg/s)"].interpolate()
+  gr["AngleX(deg)"] = gr["AngleX(deg)"].interpolate()
+  gr["AngleY(deg)"] = gr["AngleY(deg)"].interpolate()
+  gr["AngleZ(deg)"] = gr["AngleZ(deg)"].interpolate()
+  gr["T(°)"] = gr["T(°)"].interpolate()
+  gr["hx"] = gr["hx"].interpolate()
+  gr["hy"] = gr["hy"].interpolate()
+  gr["hz "] = gr["hz "].interpolate()
+
+  for idx,row in gr.iterrows():
     data["payload"]["values"].append([
-      row["ax(g)"] * 9.81,
-      row["ay(g)"] * 9.81,
-      row["az(g)"] * 9.81,
+      row["ax(g)"],
+      row["ay(g)"],
+      row["az(g)"],
       row["wx(deg/s)"],
       row["wy(deg/s)"],
       row["wz(deg/s)"],
@@ -92,9 +129,9 @@ for n, g in dfr.groupby(pandas.Grouper(key='timestamp',freq=f"{seconds}S")):
       row["AngleY(deg)"],
       row["AngleZ(deg)"],
       row["T(°)"],
-      row["hx"] / 1000000,
-      row["hy"] / 1000000,
-      row["hz "] / 1000000,
+      row["hx"],
+      row["hy"],
+      row["hz "],
     ])
 
   encoded = json.dumps(data)
